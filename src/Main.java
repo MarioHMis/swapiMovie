27a907b77dece7c916529438c22f175e0d5edb5f
MarioHMis <138615425+MarioHMis@users.noreply.github.com>
2024-10-05 09:59:01 -0600

    Init of Star wars movie API query

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        MovieQuery query = new MovieQuery();
+        System.out.println("Enter a Star Wars number movie title: ");
+
+        try {
+            var movieNumber = Integer.valueOf(sc.nextLine());
+            Movie movie = query.searchMovie(movieNumber);
+            System.out.println(movie);
+            FileGenerator generator = new FileGenerator();
+            generator.saveJson(movie);
+        } catch (NumberFormatException e) {
+            System.out.println("Invalid movie title "+ e.getMessage());
+        }
+        catch (RuntimeException | IOException e) {
+            System.out.println(e.getMessage());
+            System.out.println("Finalizing the application");
+        }
 
     }
 }
--- src/MovieQuery.java
+++ src/MovieQuery.java
@@ -1,2 +1,31 @@
+import com.google.gson.Gson;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
 public class MovieQuery {
+
+    public Movie searchMovie(int numberOfFilm) {
+        URI addrress = URI.create("https://swapi.dev/api/films/" + numberOfFilm);
+
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(addrress)
+                .build();
+
+
+        try {
+            HttpResponse<String> response = client
+                    .send(request, HttpResponse.BodyHandlers.ofString());
+            return new Gson().fromJson(response.body(), Movie.class);
+        } catch (Exception e) {
+            throw new RuntimeException("I couldn't find that movie");
+        }
+
+
+    }
+
 }
